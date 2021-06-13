19ff9cf17d70bac3472f066a6c5458f862018e41
Nikhil kumar <nikhilsingheng@gmail.com>
2021-06-14 01:32:20 +0530

    https://github.com/nikhilsingheng/test.git

---
+++ promises.js
@@ -0,0 +1,18 @@
+else
+{
+    Refect("its add number")
+}
+
+} 1000)
+)}
+}
+ data().then(Response=>{
+     console.log(Response);
+ })
+ /// I do stuff here
+
+
+
+
+ /// some promises branch stuff
+
